64eb1d3bbbeb4357f2f9b61fb33f94a1402af06e
573114r <wojtek.mendlik@gmail.com>
2023-05-04 18:35:46 +0200

    fixed objects in project

--- Assets/Wojtek/scripts/UI Scripts/KeysCollectedUI.cs
+++ Assets/Wojtek/scripts/UI Scripts/KeysCollectedUI.cs
@@ -14,11 +14,6 @@ public class KeysCollectedUI : MonoBehaviour
     public TextMeshProUGUI textElement;
     string textValue;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
 
     // Update is called once per frame
     void Update()
