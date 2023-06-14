96b6c5b324a351471eac119f89661cea2c10ef68
Wm <wojtek.mendlik@gmail.com>
2023-06-14 21:25:53 +0200

    //WIP

---
+++ Assets/Wojtek/scripts Wojciech Mendlikowski/Main Menu Scripts/MainMenuButtonsScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonsScript : MonoBehaviour
+{
+    
+    public  void OnNewGameButtonClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnLoadGameButtonClick()
+    {
+        Debug.Log("scene)");
+    }
+}
